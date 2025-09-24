15bd271e18edcf452ff912f044dc443bc506cb6d
Philo Satriya <159871132+PhiloPAS@users.noreply.github.com>
2025-09-24 20:01:40 +0700

    Complete tutorial 4: Desain Web Menggunakan HTML dan CSS3 & Metode Update dan Delete pada Data

--- main/forms.py
+++ main/forms.py
@@ -5,3 +5,8 @@ class NewsForm(ModelForm):
     class Meta:
         model = News
         fields = ["title", "content", "category", "thumbnail", "is_featured"]
+
+
+
+#@login_required(login_url='/login')
+#def show_main(request):
